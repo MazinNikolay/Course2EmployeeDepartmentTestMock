98b84688641b73ec2d248cf603fd18cb542216c8
kolia <koliamba_85@mail.ru>
2024-03-20 23:08:57 +0800

    Initial commit

---
+++ src/main/java/pro/sky/Course2EmployeeDepartmentTestMock/Course2EmployeeDepartmentTestMockApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2EmployeeDepartmentTestMock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2EmployeeDepartmentTestMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2EmployeeDepartmentTestMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Course2EmployeeDepartmentTestMock/Course2EmployeeDepartmentTestMockApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2EmployeeDepartmentTestMock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2EmployeeDepartmentTestMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
